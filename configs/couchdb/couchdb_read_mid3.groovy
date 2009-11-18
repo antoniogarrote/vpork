f77addb1cbfeed887d84be6c8b5ccc3ac52e6d28
Antonio Garrote <antonio.garrote@nb-agarrote.local>
2009-11-18 14:07:00 +0100

    test configuration files

---
+++ configs/couchdb/couchdb_read_mid3.groovy
@@ -0,0 +1,32 @@
+storageType="couchdb"
+testName="couchdb_read_mid3"
+
+/**
+ * Configuration of the Voldemort ClientStore.
+ */
+storeFactory {
+    storePort         = 8081
+    database          = "test"
+    document          = "test"
+    retries           = 5
+}
+
+// # of client threads to start
+numThreads  = 15
+
+// Odds of performing a read op per iteration (1 = 100%)
+readOdds    = 0.5
+writeOdds   = 0.5
+rewriteOdds = 0.0
+
+// How many iterations should each thread execute?
+threadIters = 4000
+
+// How much data should be written per write operation?
+dataSize    = (8 + 4) * 2000
+
+// What function best describes our data access patterns?  This factor is
+// is applied across the range of already-written records.  We choose a function
+// with a long tail, since we do most of our reads at current-time, and
+// not many in the recent past.
+readFactor = {x -> (1.0 - x) ** 10.0}
--- src/main/java/couchdbtester/Tester.java
+++ src/main/java/couchdbtester/Tester.java
@@ -7,6 +7,7 @@ package couchdbtester;
 
 import com.fourspaces.couchdb.Database;
 import com.fourspaces.couchdb.Document;
+import java.util.HashMap;
 
 /**
  *
@@ -20,6 +21,25 @@ public class Tester {
     private Document document;
     private int numTries;
     private String docKey;
+    private static HashMap<String,Document> docs;
+
+
+    public static synchronized void set_doc(String key, Document doc) {
+        if(docs==null) {
+            docs = new HashMap<String, Document>();
+        }
+        docs.put(key, doc);
+    }
+
+    public static Document get_doc(String key) {
+        if(docs==null) {
+            docs = new HashMap<String, Document>();
+            return null;
+        } else {
+            return docs.get(key);
+        }
+    }
+
 
     public Tester(com.fourspaces.couchdb.Session s, String db, String doc, Integer i) {
         this.session = s;
@@ -37,9 +57,11 @@ public class Tester {
 
     public Object get(String key) {
         try {
+            //System.out.println("r.");
             //System.out.println("Retrieving"+ key);
             this.document = this.db.getDocument(key);
             //System.out.println("KEY:"+this.docKey);
+            Tester.set_doc(key, document);
             return this.document.get(this.docKey);
         } catch (Exception e) {
             System.out.println("ERROR");
@@ -51,28 +73,35 @@ public class Tester {
     void put(String key, byte[] value) throws Exception {
         //System.out.println("Writing doc"+key);
 
-       // System.out.println("KEY:"+key);
+        //System.out.println(key+"w.");
         boolean successful = false;
         int tries = 0;
         try{
-        this.document = this.db.getDocument(key);
+            //this.document = this.db.getDocument(key);
+            this.document = Tester.get_doc(key);
+            //System.out.println(key + "rw.");
+            if(this.document==null) {
+                this.document = new Document();
+            }
         } catch(Exception ex) {
-            //System.out.println("NUEVO DOC:"+key);
+            //System.out.println("Exception:"+ex.getMessage());
             //System.out.println("Document "+key+" does not exist");
+            //System.out.println(key+"rw!.");
             this.document = new Document();
         }
         while(!successful && tries<this.numTries) {
             try {
+                //System.out.println(key+"ws.");
                 this.document.put(this.docKey, value);
                 this.db.saveDocument(this.document,key);
                 successful = true;
             } catch(Exception ex) {
-                System.out.print(".");
+                //System.out.println(key+"ws!");
                 this.document.refresh();
                 tries++;
             }
         }
-
+        Tester.set_doc(key, document);
         if(successful == false) {
             throw new Exception("Unable to store value");
         }
